8f9c0103babee1255e6fde1f0c1ad4d9ed5c5a87
Tamanna Islam <tislam143051@bscse.uiu.ac.bd>
2022-04-04 06:29:09 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,43 @@
+let weather = {
+    "apikey": "388476a9d47d3a277a69fc54fc72a3ab",
+    fetchWeather: function (city) {
+        fetch(
+            "https://api.openweathermap.org/data/2.5/weather?q="
+            + city
+            + "&units=metric&appid="
+            + this.apikey
+        )
+            .then((Response) => Response.json())
+            .then((data) => this.displayWeather(data));
+    },
+    displayWeather: function (data) {
+        const { name } = data;
+        const { icon, description } = data.weather[0];
+        const { temp, humidity } = data.main;
+        const { speed } = data.wind;
+        document.querySelector(".city").innerText = "Weather in " + name;
+        document.querySelector(".icon").src =
+            "https://openweathermap.org/img/wn/" + icon + ".png"
+        document.querySelector(".description").innerText = description;
+        document.querySelector(".temp").innerText = temp + "°C";
+        document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
+        document.querySelector(".wind").innerText = "Wind speed: " + speed + " m/s";
+        document.querySelector(".weather").classList.remove("loading");
+        document.body.style.backgroundImage = "url('https://source.unsplash.com/1600x900/?" + name + "')";
+    },
+    search: function () {
+        this.fetchWeather(document.querySelector(".search-bar").value);
+    }
+};
+
+document.querySelector(".search button").addEventListener("click", () => {
+    weather.search();
+});
+
+document.querySelector(".search-bar").addEventListener("keyup", (event) => {
+    if (event.key == "Enter") {
+        weather.search();
+    }
+});
+
+weather.fetchWeather("dhaka");
